5eaaababa658373a3fa6314287b9d8eced0095c4
Darren <darren@codingintent.com>
2022-05-30 16:42:53 +0930

    Adding in storage account and container

--- main.tf
+++ main.tf
@@ -29,3 +29,21 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+resource "azurerm_storage_account" "primary" {
+  name                     = "aucid01"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    Purpose = "learn"
+  }
+}
+
+resource "azurerm_storage_container" "audit" {
+  name                  = "request-audit-records"
+  storage_account_name  = azurerm_storage_account.primary.name
+  container_access_type = "private"
+}
+
